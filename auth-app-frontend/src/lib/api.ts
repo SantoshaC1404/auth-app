2df8e4673bdf5c01ae4bfd63a688fc3876e456e3
SantoshaC1404 <santosha1404@gmail.com>
2026-03-10 06:46:42 +0530

    auth app ui

--- auth-app-frontend/src/lib/api.ts
+++ auth-app-frontend/src/lib/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const api = axios.create({
-  baseURL: "http://localhost:8082/api",
+  baseURL: API_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
